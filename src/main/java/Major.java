ea18f20908e7310100dd2d5d488cea297a90e6a2
meowla <redmooncow@163.com>
2022-07-21 20:25:17 +0800

    init

---
+++ src/main/java/Major.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Major {
+    public static void main(String args[]){
+        int i=1;
+        Scanner sc=new Scanner(System.in);
+        while(i!=0)
+            sc.next();
+    }
+}
